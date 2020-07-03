652cd4f47b83c0ee3e93b8c5cd8b9a8ffbdc900f
Lucas <lucasramos.m@gmail.com>
2020-07-03 17:54:26 -0300

    Initial Commit

---
+++ ModuloNativoiOS/ModuloNativoiOS.h
@@ -0,0 +1,19 @@
+//
+//  ModuloNativoiOS.h
+//  ModuloNativoiOS
+//
+//  Created by Lucas on 03/07/20.
+//  Copyright © 2020 Lucas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuloNativoiOS.
+FOUNDATION_EXPORT double ModuloNativoiOSVersionNumber;
+
+//! Project version string for ModuloNativoiOS.
+FOUNDATION_EXPORT const unsigned char ModuloNativoiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuloNativoiOS/PublicHeader.h>
+
+
