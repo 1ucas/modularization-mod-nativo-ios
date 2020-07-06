a823739204307dcb4fd9cda4836f8fa3a49ca2dc
Lucas <lucasramos.m@gmail.com>
2020-07-06 10:07:35 -0300

    Versao 1 - podspec

---
+++ ModuloNativoiOS/TelaNativaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TelaNativaViewController.swift
+//  ModuloNativoiOS
+//
+//  Created by Lucas on 03/07/20.
+//  Copyright © 2020 Lucas. All rights reserved.
+//
+
+import UIKit
+
+class TelaNativaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clicouNoBotao(_ sender: UIButton) {
+        
+    }
+    
+}
